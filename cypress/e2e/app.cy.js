002dedb80f99830defe3d4d8cadb9b63179357b2
Pranav Patil <pranav.patil@paramount.com>
2025-09-25 17:09:51 +0530

    More updates7

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -13,13 +13,5 @@ describe('App E2E Tests', () => {
 
   it('should have main heading with correct text', () => {
     cy.get('h1').should('contain.text', 'Welcome to React');
-  });
-
-  it('should not have any unexpected errors in console', () => {
-    cy.on('window:before:load', (win) => {
-      cy.stub(win.console, 'error').as('consoleError');
-    });
-
-    cy.get('@consoleError').should('not.be.called');
-  });
+  })
 });
