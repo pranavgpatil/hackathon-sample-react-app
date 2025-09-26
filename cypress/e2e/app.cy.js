8196bdee497a9fe1f8fb964cb7aeb0103b22938d
Pranav Patil <github-actions[bot]@users.noreply.github.com>
2025-09-26 18:51:05 +0530

    Revert "Revert "Breaking test Angular""
    This reverts commit 1e85f49c4876e235216f20e5785fd4e4493654a7.

---
+++ cypress/e2e/app.cy.js
@@ -0,0 +1,13 @@
+describe('App E2E Tests', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('should have correct page title', () => {
+    cy.title().should('eq', 'My React App');
+  });
+
+  it('should have main heading with correct text', () => {
+    cy.get('h1').should('contain.text', 'Welcome to React');
+  })
+});
